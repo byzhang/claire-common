80551e447cfb3cb1e0f50d3b9230488e97049b92
robbinfan <fanyu83@gmail.com>
2013-12-10 16:36:27 +0800

    rm extra test file

--- claire/common/system/ProcessUtil.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CLAIRE_COMMON_BASE_PROCESSUTIL_H_
-#define _CLAIRE_COMMON_BASE_PROCESSUTIL_H_
-
-namespace claire {
-
-} // namespace clarire
-
-#endif // _CLAIRE_COMMON_BASE_PROCESSUTIL_H_
